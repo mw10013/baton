9fae5a6ca9329a63caf13efd60960b4fe225f647
Michael Wu <mw10013@gmail.com>
2026-09-08 20:01:31 -0400

    Bump @shopify/api-codegen-preset 2.0.1 to 3.0.0, align codegen schema to 2026-07

--- .graphqlrc.ts
+++ .graphqlrc.ts
@@ -4,12 +4,12 @@ import { ApiType, shopifyApiProject } from "@shopify/api-codegen-preset";
 import { ApiVersion } from "@shopify/shopify-api";
 
 const config: IGraphQLConfig = {
-  schema: `https://shopify.dev/admin-graphql-direct-proxy/${ApiVersion.January26}`,
+  schema: `https://shopify.dev/admin-graphql-direct-proxy/${ApiVersion.July26}`,
   documents: ["./src/**/*.{js,ts,jsx,tsx}"],
   projects: {
     default: shopifyApiProject({
       apiType: ApiType.Admin,
-      apiVersion: ApiVersion.January26,
+      apiVersion: ApiVersion.July26,
       documents: ["./src/**/*.{js,ts,jsx,tsx}"],
       outputDir: "./.codegen",
     }),
